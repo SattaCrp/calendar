83e42eef2646a016a687e862d9d020a9dbc7eb28
SattaCrp <SattaCrp@github.com>
2017-08-31 21:48:22 +0700

    Delete useless code

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -11,10 +11,8 @@ public class MainController {
 	private ArrayList<Memo> memoList;
 
 	public void startApplication() {
-//		JOptionPane.showMessageDialog(null, "Calendar Memo \n step 1: chose month and year \n step 2: press ShowDays button"+ "\n step 3: chose days and press ShowMemo button \n step 4: Write a note \n step 5: press Sumbit button");
 		view = new View();
 		memoList = new ArrayList<>();
-		
 		view.initFream();
 		view.getBtnShow().addActionListener(new ShowActionlistener(view));
 		view.getBtnSub().addActionListener(new SubmitAc(view, memoList));
